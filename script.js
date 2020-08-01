9583e38de7c4c6e2bf74ba86e486ae1e700a3fe4
Olesya Irkhina <itekkie888@gmail.com>
2020-08-01 19:36:12 -0400

    add comment

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const close = document.getElementById("close");
 const open = document.getElementById("open");
+const comt = document.getElementById("comt");
 const modal = document.getElementById("modal");
+const comcont = document.getElementById("comcont");
 
 document.querySelector('.menu-btn').addEventListener('click', () => document.querySelector('.main-menu').classList.toggle('show'));
 
@@ -13,4 +15,15 @@ close.addEventListener("click", () => modal.classList.remove("show-modal"));
 // Hide modal on outside click
 window.addEventListener("click", (e) =>
     e.target == modal ? modal.classList.remove("show-modal") : false
+);
+
+// Show comcont
+comt.addEventListener("click", () => comcont.classList.add("show-comcont"));
+
+// Hide comcont
+close.addEventListener("click", () => comcont.classList.remove("show-comcont"));
+
+// Hide comcont on outside click
+window.addEventListener("click", (e) =>
+    e.target == comcont ? comcont.classList.remove("show-comcont") : false
 );
